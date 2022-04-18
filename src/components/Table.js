f93b74fd48a7e001a7302510fc0d864f42a36409
Nhatdev94 <nhatdev94@gmail.com>
2022-04-19 06:39:42 +0700

    fix tao moi learning khi hoc thuoc 6 tu

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,18 +5,20 @@ function Table({ data, learning = false }) {
   return (
     <div className="table w-full">
       <div className="flex items-center pt-2 border-b py-2">
-        <p className="w-1/3 font-bold">Vocabulary</p>
-        <p className="w-2/3 font-bold">Mean</p>
+        <p className="w-1/5 font-bold">Vocabulary</p>
+        <p className="w-1/5 font-bold">LearnNum</p>
+        <p className="w-3/5 font-bold">Mean</p>
       </div>
       {data?.map((item, i) => {
         return (
           <div className="flex items-center" key={i}>
-            <p className={`w-1/3 
+            <p className={`w-1/5 
                             font-semibold 
                             py-2 border-b
                             ${!learning ? item.type : ''}
                             `}>{item?.vocabulary}</p>
-            <p className="w-2/3 py-2 border-b">{item?.mean}</p>
+            <p className="w-1/5 py-2 border-b">{item?.learnNum}</p>
+            <p className="w-3/5 py-2 border-b">{item?.mean}</p>
           </div>
         );
       })}
